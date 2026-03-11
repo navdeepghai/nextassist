6e137786cfbc059aea0137985fc2309ab59097ce
Navdeep Singh <navdeepghai1@gmail.com>
2026-03-11 01:28:10 +0000

    fixes for userguide

--- nextassist/www/userguide.py
+++ nextassist/www/userguide.py
@@ -6,3 +6,7 @@ no_cache = 1
 def get_context(context):
 	context.no_cache = 1
 	context.title = "NextAssist User Guide"
+	context.no_header = True
+	context.no_breadcrumbs = True
+	context.show_sidebar = False
+	context.full_width = True
